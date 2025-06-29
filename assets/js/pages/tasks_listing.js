9d85075a76a9d600af33700ec1dd0f4e312cbc96
SimpleGuy <reeaz@netsiteweaver.com>
2025-06-29 09:55:13 +0400

    prevent autofill in task listing for search

--- assets/js/pages/tasks_listing.js
+++ assets/js/pages/tasks_listing.js
@@ -286,11 +286,11 @@ jQuery(function(){
         let display = $('#display').val();
         let assigned_to = $('#assigned_to').val();
         let notes_only = $('#notes_only').val();
-        let search_text = $('#search_text').val();
+        let tasks_search_text = $('#tasks_search_text').val();
 
         Overlay("on");
         setTimeout(function(){
-            window.location.href = '/tasks/listing?customer_id='+customer_id+"&project_id="+project_id+"&sprint_id="+sprint_id+"&stage="+stage+"&order_by="+order_by+"&order_dir="+order_dir+"&display="+display+"&assigned_to="+assigned_to+"&notes_only="+notes_only+"&search_text="+search_text;
+            window.location.href = '/tasks/listing?customer_id='+customer_id+"&project_id="+project_id+"&sprint_id="+sprint_id+"&stage="+stage+"&order_by="+order_by+"&order_dir="+order_dir+"&display="+display+"&assigned_to="+assigned_to+"&notes_only="+notes_only+"&search_text="+tasks_search_text;
         },100)
     })
 
@@ -341,7 +341,7 @@ jQuery(function(){
         let display = $('#display').val();
         let assigned_to = $('#assigned_to').val();
         let notes_only = $('#notes_only').val();
-        let search_text = $('#search_text').val();
+        let tasks_search_text = $('#tasks_search_text').val();
 
         let selectedStages = [];
         $('#stages-list li.selected').each(function(i,j){
@@ -354,7 +354,7 @@ jQuery(function(){
 
         Overlay("on");
         setTimeout(function(){
-            window.location.href = '/tasks/listing?customer_id='+customer_id+"&project_id="+project_id+"&sprint_id="+sprint_id+"&stage="+JSON.stringify(selectedStages)+"&order_by="+order_by+"&order_dir="+order_dir+"&display="+display+"&assigned_to="+assigned_to+"&notes_only="+notes_only+"&search_text="+search_text;
+            window.location.href = '/tasks/listing?customer_id='+customer_id+"&project_id="+project_id+"&sprint_id="+sprint_id+"&stage="+JSON.stringify(selectedStages)+"&order_by="+order_by+"&order_dir="+order_dir+"&display="+display+"&assigned_to="+assigned_to+"&notes_only="+notes_only+"&search_text="+tasks_search_text;
 
             // $('.monitor').trigger("change")
         },100)
