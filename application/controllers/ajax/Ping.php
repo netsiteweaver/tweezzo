8ee906fd05124d7c54e5992e3d6c45566a657856
SimpleGuy <reeaz@netsiteweaver.com>
2025-10-23 11:40:56 +0400

    remove flicker on session ping

--- application/controllers/ajax/Ping.php
+++ application/controllers/ajax/Ping.php
@@ -1,8 +1,21 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Ping extends CI_Controller
 {
+    /**
+     * Ping endpoint to check if user session is still active
+     * Called every 5 seconds by main.js isLoggedIn() function
+     * 
+     * If user is not logged in, the Auth hook will catch it and return error
+     * If we reach here, user is logged in, so return success
+     */
     function index()
     {
-        
+        echo json_encode(array(
+            "result" => true,
+            "user_id" => isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null,
+            "timestamp" => date('Y-m-d H:i:s')
+        ));
     }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -26,13 +26,33 @@ $(window).on('load', function() {
 	Overlay('off');
 });
 
-// Show loader during AJAX requests
-$(document).ajaxStart(function() {
-	Overlay('on');
+// Track active AJAX requests that should show loader
+var activeLoaderRequests = 0;
+
+// Show loader during AJAX requests (except background requests)
+$(document).ajaxSend(function(event, jqxhr, settings) {
+	// Exclude background requests from showing loader
+	if (settings.showLoader !== false && 
+		!settings.url.includes('ajax/ping') && 
+		!settings.url.includes('isLoggedIn')) {
+		activeLoaderRequests++;
+		if (activeLoaderRequests > 0) {
+			Overlay('on');
+		}
+	}
 });
 
-$(document).ajaxStop(function() {
-	Overlay('off');
+$(document).ajaxComplete(function(event, jqxhr, settings) {
+	// Only hide loader if this was a request that showed it
+	if (settings.showLoader !== false && 
+		!settings.url.includes('ajax/ping') && 
+		!settings.url.includes('isLoggedIn')) {
+		activeLoaderRequests--;
+		if (activeLoaderRequests <= 0) {
+			activeLoaderRequests = 0;
+			Overlay('off');
+		}
+	}
 });
 
 $(document).ready(function(){
